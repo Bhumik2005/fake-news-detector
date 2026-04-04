1d029f643678860c58b99b27d1a8f76236558d02
Bhumik Kumta <bhumik.kumta23@pcu.edu.in>
2026-04-05 01:53:49 +0530

    made some few changes in streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -4,6 +4,9 @@ import requests
 # Page config
 st.set_page_config(page_title="Fake News Detector", layout="centered")
 
+# API URL (DEPLOYED BACKEND)
+API_URL = "https://fake-news-detector-1-lc2z.onrender.com/predict"
+
 # Title
 st.title("📰 Fake News Detector")
 st.write("Enter a news article below to check if it's real or fake.")
@@ -16,30 +19,23 @@ if st.button("Predict"):
 
     if text.strip() == "":
         st.warning("⚠️ Please enter some text!")
-    
+
     else:
-        # 🔥 Warning for short input
         if len(text.split()) < 20:
             st.warning("⚠️ Please enter a longer article for better accuracy")
 
         try:
-            # API call
-            response = requests.post(
-                "http://127.0.0.1:8000/predict",
-                json={"text": text}
-            )
+            response = requests.post(API_URL, json={"text": text})
 
             if response.status_code == 200:
                 data = response.json()
 
-                # 🔥 Prediction Result
                 st.subheader("Result")
                 if data["prediction"] == 1:
                     st.success("✅ Real News")
                 else:
                     st.error("❌ Fake News")
 
-                # 🔥 Confidence Display
                 st.subheader("Prediction Confidence")
 
                 st.write("Real News Confidence")
@@ -50,7 +46,6 @@ if st.button("Predict"):
                 st.progress(int(data["confidence_fake"] * 100))
                 st.caption(f"{data['confidence_fake']*100:.1f}%")
 
-                # 🔥 Related Articles Section
                 st.subheader("📰 Related News Articles")
 
                 articles = data.get("related_articles", [])
@@ -65,7 +60,7 @@ if st.button("Predict"):
                     st.write("No related news found.")
 
             else:
-                st.error("❌ API Error. Make sure FastAPI is running.")
+                st.error("❌ API Error")
 
         except Exception as e:
             st.error("❌ Could not connect to backend.")
